c1a14e3748f5ef0ed126cc586b85f171e9de71f4
Stumpp <kiril.bychkov.03@mail.ru>
2023-09-26 20:40:57 +0300

    deleted back

--- back/src/main/java/com/example/hospital/repository/InstructionRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.hospital.repository;
-
-import com.example.hospital.model.Instruction;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface InstructionRepository extends JpaRepository<Instruction,Long> {
-    Instruction getInstructionByTitle(String title);
-}
